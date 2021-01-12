f7ef0da6b19ecc4e192bf0d59b5bb4e62007af96
Syuzan <syuzanna.ant@gmail.com>
2021-01-12 10:39:59 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Form from "./containers/Form/Form";
+
 import './App.css';
 
 
 function App() {
   return (
     <div className="App container">
-      <Form />
+      
     </div>
   );
 }
